8444ecdf12d9106c13e86daf0348c96036dc7ee4
yunjinchoi <yunjinchoi@kr.kpmg.com>
2024-02-19 17:32:03 +0900

    [ADD] front - router, header

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,8 +4,10 @@ import AuthCallback from 'pages/AuthCallback';
 import Element from 'pages/Element';
 import Env from 'pages/Env';
 import Home from 'pages/Home';
+import HomeLighthouse from 'pages/HomeLighthouse';
 import Login from 'pages/Login';
 import Readme from 'pages/Readme';
+import ReadmeLighthouse from 'pages/ReadmeLighthouse';
 import Thread from 'pages/Thread';
 
 export const router = createBrowserRouter([
@@ -16,6 +18,14 @@ export const router = createBrowserRouter([
   {
     path: '/readme',
     element: <Readme />
+  },
+  {
+    path: '/custom',
+    element: <HomeLighthouse />
+  },
+  {
+    path: '/custom_readme',
+    element: <ReadmeLighthouse />
   },
   {
     path: '/env',
