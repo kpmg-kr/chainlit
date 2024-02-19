41e94308060763b71770bbce16d932022b2ba381
yunjinchoi <yunjinchoi@kr.kpmg.com>
2024-02-19 17:37:37 +0900

    [ADD] front - chat page

---
+++ frontend/src/pages/HomeLighthouse.tsx
@@ -0,0 +1,11 @@
+import PageLighthouse from 'pages/PageLighthouse';
+
+import Chat_Lighthouse from 'components/organisms/chat/indexLighthouse';
+
+export default function HomeLighthouse() {
+  return (
+    <PageLighthouse>
+      <Chat_Lighthouse />
+    </PageLighthouse>
+  );
+}
---
+++ frontend/src/pages/ReadmeLighthouse.tsx
@@ -0,0 +1,27 @@
+import { useRecoilValue } from 'recoil';
+
+import Page from 'pages/Page';
+
+import Box from '@mui/material/Box';
+
+import WelcomeScreen from 'components/organisms/chat/Messages/welcomeScreen';
+
+import { projectSettingsState } from 'state/project';
+
+export default function ReadmeLighthouse() {
+  const projectSettings = useRecoilValue(projectSettingsState);
+
+  return (
+    <Page>
+      <Box my={2} display="flex" flexGrow={1}>
+        <WelcomeScreen
+          variant="app"
+          // Lighthouse - Custom Text
+          markdown={projectSettings?.markdown_lighthouse}
+          allowHtml={projectSettings?.features?.unsafe_allow_html}
+          latex={projectSettings?.features?.latex}
+        />
+      </Box>
+    </Page>
+  );
+}
